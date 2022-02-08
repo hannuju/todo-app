ffdebc5d2ba4e896b7ca9ee09b84a809145c4892
Hannu Junno <hannu.junno@gmail.com>
2022-02-08 17:09:34 +0200

    move front end files to own directory

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const path = require("path");
 const app = express();
 const PORT = 9000;
 
-app.use(express.static(path.join(__dirname, "..", "build")));
+app.use(express.static(path.join(__dirname, "..", "front-end", "build")));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "front-end", "index.html"));
 });
 
 app.listen(PORT);
