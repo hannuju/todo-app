db8ca8bfc7ed795d5730acee9609005520f5a168
Hannu Junno <hannu.junno@gmail.com>
2022-02-08 12:59:25 +0200

    add checkbox

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './TodoItem.css'
 
 interface Props {
@@ -6,8 +7,16 @@ interface Props {
 }
 
 export const TodoItem: React.FC<Props> = (props) => {
+
+    const [done, setDone] = useState(props.checkbox);
+
+    const checkboxChangeHandler = () => {
+        done ? setDone(false) :  setDone(true);
+    }
+
     return (
-        <div className="todo-item">
+        <div className={`todo-item ${done && "done"}`}>
+            <input type="checkbox" defaultChecked={done} onChange={checkboxChangeHandler}/>
             <p>{props.title}</p>
         </div>
     )
